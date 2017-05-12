7843b5005bc4636730fcb9b120a3d562537fb305
Amr Hossam <amrhossam902@gmail.com>
2017-05-12 14:29:51 -0700

    subtract contains zeros

--- SynthesisCode/subtract_module.vhd
+++ SynthesisCode/subtract_module.vhd
@@ -3,6 +3,7 @@ use ieee.std_logic_1164.all;
 
 entity sub is
 	port(A, B: in std_logic_vector(17 downto 0);
+	     zero, one: in std_logic;
 	     sub_value: out std_logic_vector(17 downto 0));
 end sub;
 
@@ -14,7 +15,7 @@ architecture sub_ARC of sub is
 component nbit_adder IS
        GENERIC (n : integer := 16);
 PORT(a,b : IN std_logic_vector(n-1  DOWNTO 0);
-             cin : IN std_logic;  
+             cin: IN std_logic;  
             add_out : OUT std_logic_vector(n-1 DOWNTO 0));
 END component;
 
@@ -31,11 +32,14 @@ END component;
 --signals
 signal B_bar : std_logic_vector(17 downto 0);
 signal Adder1_value, Adder2_value,Adder1_value_bar : std_logic_vector(17 downto 0);
-
+signal zeros: std_logic_vector(17 downto 0);
 begin
+
+zeros <= zero &zero &zero &zero &zero &zero &zero &zero &zero &zero &zero &zero &zero &
+	zero &zero &zero &zero &zero;
 	
-Adder1_sub: nbit_adder generic map(18) port map(A, B_bar, '1', Adder1_value);
-Adder2_sub: nbit_adder generic map(18) port map(Adder1_value_bar, "000000000000000000", '1', Adder2_value);	
+Adder1_sub: nbit_adder generic map(18) port map(A, B_bar, one, Adder1_value);
+Adder2_sub: nbit_adder generic map(18) port map(Adder1_value_bar, zeros, one, Adder2_value);	
 
 B_bar <= not B;
 Adder1_value_bar <= not Adder1_value;
